ee7d50ba9d97c7f6e25d66543aceb104ebf7ef82
ABHISHEK SAHU <kalisahu752@gmail.com>
2024-03-23 16:17:25 +0530

    utils code  and API Errors And Response

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+  path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
 
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECTION FAILED: ", error);
+  });
 
 /*
-import express from "express";
-const app = express();
 ;( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
